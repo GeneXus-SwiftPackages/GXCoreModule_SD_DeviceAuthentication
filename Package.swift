57b096f5d13f4f959945a5ed5d483e61238d305e
The iOS Team @ GeneXus <builder@genexus.com>
2026-06-19 17:51:55 -0400

    Package version: 4.2.0-beta.23

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
 			targets: ["GXCoreModule_SD_DeviceAuthenticationWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.22")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.2.0-beta.23")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_DeviceAuthenticationWrapper",
@@ -21,8 +21,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_DeviceAuthentication",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_DeviceAuthentication-4.2.0-beta.22.xcframework.zip",
-			checksum: "0b929040bfd369b064d5cadeb8a8bbedccc11f43bbaea18c9a4dd7d0e0294b5f"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_DeviceAuthentication-4.2.0-beta.23.xcframework.zip",
+			checksum: "8d09a26662f74e0a9ee8336c4991d261ed04ca32c01c54793dabc42a70b814a4"
 		)
 	]
 )
